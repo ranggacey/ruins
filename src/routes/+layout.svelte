11d5cce1e139f94c2dfe9a63f9d3e76ba1e175b9
Yuna <yuna@hermes.ai>
2026-08-19 13:04:38 +0000

    Add Tailwind CSS config and layout import

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import '../app.css';
+  let { children } = $props();
+</script>
+
+<div class="min-h-screen bg-[#050508] text-amber-50 font-serif antialiased">
+  {@render children()}
+</div>
